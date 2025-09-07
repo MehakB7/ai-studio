577c4b3451d3ae1194b0fb526cfaaa6141c68b56
mehakb7 <mehak@founderandlightning.com>
2025-09-07 23:19:52 +0530

    chore: added jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
